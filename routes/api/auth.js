cf113568ececb536b95f9ac04d17585f0ef7729b
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2024-05-21 17:33:40 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,7 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/auth", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
-router.get("/username/:username", authenticate, ctrl.findByUsername);
+router.get("/username/:username", ctrl.findByUsername);
 router.get("/email/:email", authenticate, ctrl.findByEmail);
 
 module.exports = router;
